7bbd22e5fed82391674d491d9c807c006338b4f8
maxim <maxim@DESKTOP-K4HQUNE>
2021-11-25 20:29:50 -0300

    avance agregar dueño

--- src/controlador/RegistroDueño.java
+++ src/controlador/RegistroDueño.java
@@ -1,8 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package controlador;
 
 import bd.Conexion;
@@ -39,13 +36,68 @@ public class RegistroDueño {
             return true;
 
         } catch (SQLException e) {
-            System.out.println("Error SQL al agregar Datos del Dueño");
+            System.out.println("Error SQL al agregar datos del dueño");
             return false;
         } catch (Exception e) {
-            System.out.println("Error al agregar datos del Dueño");
+            System.out.println("Error al agregar datos del dueño");
             return false;
         }
 
     }
 
+    public boolean actualizar(Dueño dueño) {
+
+        try {
+            Conexion con = new Conexion();
+            Connection cnt = con.obtenerConexion();
+            
+            String query = "UPDATE dueño SET rut=?, nombre=?, apellido=?, direccionCalle=?, direccionNro=?, telefono=? WHERE rut=?";
+            PreparedStatement stmt = cnt.prepareStatement(query);
+
+            stmt.setInt(1, dueño.getRut());
+            stmt.setString(2, dueño.getNombre());
+            stmt.setString(3, dueño.getApellido());
+            stmt.setString(4, dueño.getDireccionCalle());
+            stmt.setInt(5, dueño.getDireccionNro());
+            stmt.setInt(6, dueño.getTelefono());
+
+            stmt.executeUpdate();
+            stmt.close();
+            cnt.close();
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error SQL al actualizar datos del dueño");
+            return false;
+        } catch (Exception e) {
+            System.out.println("Error al actualizar datos del dueño");
+            return false;
+        }
+
+    }
+
+    public boolean eliminar(int rut) {
+
+        try {
+            Conexion con = new Conexion();
+            Connection cnt = con.obtenerConexion();
+
+            String query = "DELETE FROM dueño WHERE rut=?";
+            PreparedStatement stmt = cnt.prepareStatement(query);
+            stmt.setInt(1, rut);
+
+            stmt.executeUpdate();
+            stmt.close();
+            cnt.close();
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error SQL al eliminar datos del dueño");
+            return false;
+        } catch (Exception e) {
+            System.out.println("Error al eliminar datos del dueño");
+            return false;
+        }
+
+    }
 }
