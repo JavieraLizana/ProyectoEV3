6de8e1e5fe03f44c425d773ccc19fc82e65cc1b3
javie <javie@LAPTOP-4C6SKMIF>
2021-11-25 15:46:45 -0300

    Clase conexion lista

--- src/bd/Conexion.java
+++ src/bd/Conexion.java
@@ -2,10 +2,32 @@
 
 package bd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 /**
  *
- * @author javiera
+ * @author javiera Lizana
+ * @version 25-11-2021
  */
 public class Conexion {
     
+    public Connection obtenerConexion(){
+        
+        Connection connection = null;
+        
+        try {
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/veterinaria","root","");
+            System.out.println("Conexión exitosa");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error de conexión (En la clase Conexión)" + e.getMessage());
+        }
+        return connection;
+    }
 }
+
+
