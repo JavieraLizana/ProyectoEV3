7b2c6d9c7a066a2c0ae477475d405dd45e9e5207
javie <javie@LAPTOP-4C6SKMIF>
2021-11-25 15:56:53 -0300

    clase Paciente

--- src/bd/Conexion.java
+++ src/bd/Conexion.java
@@ -1,33 +1,31 @@
-
-
 package bd;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 /**
  *
- * @author javiera Lizana
+ * @author javiera Lizana, Colaboración: Maximiliano Lasmar
  * @version 25-11-2021
  */
 public class Conexion {
-    
-    public Connection obtenerConexion(){
-        
+
+    public Connection obtenerConexion() {
+
         Connection connection = null;
-        
+
         try {
-            
+
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/veterinaria","root","");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/veterinaria", "root", "");
             System.out.println("Conexión exitosa");
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Error de conexión (En la clase Conexión)" + e.getMessage());
         }
         return connection;
     }
+
 }
 
 
---
+++ src/modelo/Paciente.java
@@ -0,0 +1,12 @@
+
+
+package modelo;
+
+/**
+ *
+ * @author javiera Lizana, Colaboración: Maximiliano Lasmar
+ * @version 25-11-2021
+ */
+public class Paciente {
+    
+}
