a2e5351d31baad7d65ccc07641f4bbca3ada5669
javie <javie@LAPTOP-4C6SKMIF>
2021-11-25 20:14:38 -0300

    Merge origin/master
    Conflicts:
    	src/controlador/Registro.java
    	src/modelo/Dueño.java

--- src/controlador/RegistroPaciente.java
+++ src/controlador/RegistroPaciente.java
@@ -32,7 +32,7 @@ public class RegistroPaciente {
             stmt.setString(4, paciente.getRaza());
             stmt.setInt(5, paciente.getEdad());
             stmt.setInt(6, paciente.getPeso());
-            stmt.setString(7, String.valueOf (paciente.getSexo()));
+            stmt.setString(7, String.valueOf(paciente.getSexo()));
             stmt.setString(8, paciente.getProcedimiento());
 
             stmt.executeUpdate();
@@ -50,5 +50,70 @@ public class RegistroPaciente {
 
     }
 
-}
+    public boolean actualizar(Paciente paciente) {
 
+        Date date;
+
+        try {
+
+            Conexion conx = new Conexion();
+            Connection cnx = conx.obtenerConexion();
+
+            date = paciente.getFechaConsulta();
+
+            String query = "UPDATE paciente SET fechaConsulta=?, nombreMascota=?, especie=?, raza=?, edad, peso=?, sexo=?, procedimiento=? WHERE idConsulta=?";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setDate(1, (java.sql.Date) paciente.getFechaConsulta());
+            stmt.setString(2, paciente.getNombreMascota());
+            stmt.setString(3, paciente.getEspecie());
+            stmt.setString(4, paciente.getRaza());
+            stmt.setInt(5, paciente.getEdad());
+            stmt.setInt(6, paciente.getPeso());
+            stmt.setString(7, String.valueOf(paciente.getSexo()));
+            stmt.setString(8, paciente.getProcedimiento());
+            stmt.setInt(9, paciente.getIdConsulta());
+
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error SQL al actualizar paciente");
+            return false;
+        } catch (Exception e) {
+            System.out.println("Error al actualizar paciente");
+            return false;
+
+        }
+
+    }
+
+    public boolean eliminar(int idConsulta) {
+
+        try {
+
+            Conexion conx = new Conexion();
+            Connection cnx = conx.obtenerConexion();
+
+            String query = "DELETE FROM paciente WHERE idConsulta=?";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setInt(1, idConsulta);
+
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error SQL al eliminar paciente");
+            return false;
+        } catch (Exception e) {
+            System.out.println("Error al eliminar paciente");
+            return false;
+
+        }
+
+    }
+    
+}
