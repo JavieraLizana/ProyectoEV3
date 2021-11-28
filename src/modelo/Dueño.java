aabd125dc70bef48d043fdb0aca21a6adc3b9185
javie <javie@LAPTOP-4C6SKMIF>
2021-11-27 21:04:15 -0300

    listo

--- src/modelo/Dueño.java
+++ src/modelo/Dueño.java
@@ -1,8 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package modelo;
 
 /**
@@ -11,21 +8,42 @@ package modelo;
  */
 public class Dueño {
     
+    private int rut;
+    private int idConsulta;
     private String nombre, apellido, direccionCalle;
-    private int rut, direccionNro, telefono,idConsulta, valor;
+    private int direccionNro, telefono;
+    private String tipoPago;
+    private int valor;
 
     public Dueño() {
     }
 
-    public Dueño(String nombre, String apellido, String direccionCalle, int rut, int direccionNro, int telefono, int idConsulta, int valor) {
+    public Dueño(int rut, int idConsulta, String nombre, String apellido, String direccionCalle, int direccionNro, int telefono, String tipoPago, int valor) {
+        this.rut = rut;
+        this.idConsulta = idConsulta;
         this.nombre = nombre;
         this.apellido = apellido;
         this.direccionCalle = direccionCalle;
-        this.rut = rut;
         this.direccionNro = direccionNro;
         this.telefono = telefono;
-        this.idConsulta = idConsulta;
+        this.tipoPago = tipoPago;
         this.valor = valor;
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+
+    public int getIdConsulta() {
+        return idConsulta;
+    }
+
+    public void setIdConsulta(int idConsulta) {
+        this.idConsulta = idConsulta;
     }
 
     public String getNombre() {
@@ -52,14 +70,6 @@ public class Dueño {
         this.direccionCalle = direccionCalle;
     }
 
-    public int getRut() {
-        return rut;
-    }
-
-    public void setRut(int rut) {
-        this.rut = rut;
-    }
-
     public int getDireccionNro() {
         return direccionNro;
     }
@@ -76,12 +86,12 @@ public class Dueño {
         this.telefono = telefono;
     }
 
-    public int getIdConsulta() {
-        return idConsulta;
+    public String getTipoPago() {
+        return tipoPago;
     }
 
-    public void setIdConsulta(int idConsulta) {
-        this.idConsulta = idConsulta;
+    public void setTipoPago(String tipoPago) {
+        this.tipoPago = tipoPago;
     }
 
     public int getValor() {
